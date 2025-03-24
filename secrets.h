0f6fbb7ccbe7b82007b3baaa5d89867ae96493f2
Daksh0102 <daksh2005hora@gmail.com>
2025-03-24 22:48:03 +1100

    Create secrets.h

---
+++ secrets.h
@@ -0,0 +1,12 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+// WiFi credentials
+#define SECRET_SSID "Daksh"   // WiFi network name
+#define SECRET_PASS "hehe123456"   // WiFi password
+
+// ThingSpeak credentials
+#define SECRET_CH_ID 2889865      // ThingSpeak Channel ID
+#define SECRET_WRITE_APIKEY "IAPVIHXFN1TUQUOM" // ThingSpeak Write API Key
+
+#endif // SECRETS_H
